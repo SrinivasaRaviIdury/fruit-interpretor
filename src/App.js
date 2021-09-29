7f5e43cd68d61da22578b524aa716f58e1635741
SrinivasaRaviAhlad <42990095+raviaahlad@users.noreply.github.com>
2021-09-29 17:26:48 +0530

    code : added fruits feature

--- src/App.js
+++ src/App.js
@@ -1,47 +1,30 @@
 import React, { useState } from "react";
 import "./styles.css";
-
-/**
- * concept notes: styling in react
- */
-
-/**
- * concept of hashmap (object as hashmap)
- * O(1) and why we are doing this.
- */
-const emojiDictionary = {
-  "😊": "Smiling",
-  "😳": "disbelief",
-  "😔": "sad",
-  "🥡": "takeout box",
-  "❤️": "love",
-  "😑":
-    "annoyance" /** add some more to show how the app now expands when there's new data */
+const fruitDictionary = {
+  "🍇": "grape",
+  "🍉": "melon",
+  "🍒": "cherry",
+  "🍓": "strawberry",
+  "🍎": "apple",
+  "🍍":
+    "pine apple" 
 };
 
-/**
- * Bonus feature
- * converting an object to array of keys
- */
-const emojis = Object.keys(emojiDictionary);
+const fruits = Object.keys(fruitDictionary);
 
 export default function App() {
-  const [emoji, setEmoji] = useState(""); /** concept 2 is useState */
+  const [fruit, setFruit] = useState(""); 
   const [meaning, setMeaning] = useState("translation will appear here..");
 
   function changeHandler(event) {
-    const inputEmoji = event.target.value;
-    setEmoji(inputEmoji);
-
-    if (inputEmoji in emojiDictionary) {
-      setMeaning(emojiDictionary[inputEmoji]);
-    } else {
-      setMeaning("failure to recognise this emoji");
+    var inputFruit = event.target.value;
+    setFruit(inputFruit);
+    if (inputFruit in fruitDictionary) {
+      setMeaning(fruitDictionary[inputFruit]);
     }
   }
-
-  function emojiClickHandler(inputEmoji) {
-    setMeaning(emojiDictionary[inputEmoji]);
+  function fruitClickHandler(fruit) {
+    setMeaning(fruitDictionary[fruit]);
   }
 
   return (
@@ -50,28 +33,24 @@ export default function App() {
       <h1>inside outttttt</h1>
       <input
         onChange={changeHandler}
-        value={emoji}
-        placeholder={"Search your emoji"}
+        value={fruit}
+        placeholder={"Search your Fruit"}
         style={{
           padding: "1em",
           minWidth: "80%"
         }}
       />
-      <h2> {emoji} </h2> {/** Concept 1: JSX */}
-      <h3> {meaning} </h3> {/** how much part is re-rendered. */}
+      <h2> {fruit} </h2> 
+      <h3> {meaning} </h3> 
       {
-        /** Bonus feature; if time permmits */
-        /**
-         * concepts to cover: mapping a list
-         * click handler on list item
-         */
-        emojis.map((emoji) => (
+        
+        fruits.map((fruit) => (
           <span
-            onClick={() => emojiClickHandler(emoji)}
+            onClick={() => fruitClickHandler(fruit)}
             style={{ fontSize: "2rem", padding: "0.5rem", cursor: "pointer" }}
           >
             {" "}
-            {emoji}{" "}
+            {fruit}{" "}
           </span>
         ))
       }
